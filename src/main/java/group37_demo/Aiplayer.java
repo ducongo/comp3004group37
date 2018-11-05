1511c215e6b95101dce1fb961489f626233e1eb0
jdn294615 <jdn294615@hotmail.com>
2018-11-05 02:17:57 -0500

    main: modified Aiplayer

--- src/main/java/group37_demo/Aiplayer.java
+++ src/main/java/group37_demo/Aiplayer.java
@@ -6,6 +6,10 @@ public class Aiplayer extends Player implements Observer{
 	
 	private Board board;
 	private Deck deck;
+	/*private ArrayList<Integer> indexes;
+	private ArrayList<Tile> group;
+	*/
+	
 	
 	public Aiplayer(ArrayList<Tile> tiles, String name, Board board, Deck deck) {
 		
@@ -15,11 +19,24 @@ public class Aiplayer extends Player implements Observer{
 		this.deck = deck;
 	}
 	
-	public void play() {
+	public void play(ArrayList<Integer> indexes, ArrayList<Tile> group, Tile tile) {
 		
-		System.out.println("Strategy: " + name + " is playing");
+		System.out.println("Player p1's turn ");
 		this.hand.addTile(deck.dealTile());
 		
+		this.hand.createMeld(indexes);
+		if(this.hand.getMeldValue() >= 30) {
+			board.addGroup(group);
+		}else {
+			this.hand.addTile(tile);
+		}
+		
+	}
+	
+	public void Ai1Win() {
+		if (this.hand.isEmpty()) {
+			System.out.println("AI 1 is winner");
+		}
 	}
 	
 	
